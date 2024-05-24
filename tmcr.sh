557ee7cd603f7c3e88789bbb0610f3e28148290c
Rafi98 <33840574+Rafi98@users.noreply.github.com>
2024-05-24 23:42:20 +0530

    Update tmcr.sh

--- tmcr.sh
+++ tmcr.sh
@@ -14,7 +14,8 @@ org="RafiCisco"
 # Raw URL of the JSON file
 #json_url="https://raw.githubusercontent.com/$org/$repo/$branch/$json_path"
 
-json_file="https://github.com/RafiCisco/projA/blob/main/repos.json"
+#json_file="https://github.com/RafiCisco/projA/blob/main/repos.json"
+json_file=repos.json
 
 # Check if JSON file exists
 if [ ! -f "$json_file" ]; then
