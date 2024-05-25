0136f3cf16399d0c92f00a8973d7a9206045abf7
Rafi98 <33840574+Rafi98@users.noreply.github.com>
2024-05-25 14:10:14 +0530

    Update tmcr.sh

--- tmcr.sh
+++ tmcr.sh
@@ -12,7 +12,7 @@ GITHUB_TOKEN="${GITHUB_TOKEN}"
 TEAM_NAMES=("admin" "dev")
 TEAM_DESCRIPTIONS=("Admin team with full access" "Development team with write access")
 TEAM_PRIVACY="closed"  # or "secret"
-REPOSITORIES=("projA/rp1" "projA/rp2")  # Full names of repositories under projA
+REPOSITORIES=("projA")  # Full names of repositories under projA
 
 # Function to check if a team exists
 team_exists() {
