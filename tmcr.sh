8b82eaa66607cff68a1410993e73adfb37892d8b
Rafi98 <33840574+Rafi98@users.noreply.github.com>
2024-05-27 13:17:28 +0530

    Update tmcr.sh

--- tmcr.sh
+++ tmcr.sh
@@ -2,8 +2,8 @@
 
 # Read the JSON file and extract repository information
 json_file="repos.json"
-project_name=$(jq -r '.ProjA.name' "$json_file")
-sub_repos=$(jq -c '.ProjA.sub_repos[]' "$json_file")
+project_name=$(jq -r '.projA.name' "$json_file")
+sub_repos=$(jq -c '.projA.sub_repos[]' "$json_file")
 
 echo "Project: $project_name"
 
